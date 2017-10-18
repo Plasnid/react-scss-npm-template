0cc0d38841d73520b8364caf759e35188edf05c5
Stuart Sackler <stuart.sackler@gmail.com>
2017-10-18 12:51:06 -0400

    first commit of the webpack template

---
+++ src/scripts/main.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import style from '../styles/main.scss';
+
+ReactDOM.render(<h1 className="title">I am the king of the squirrels!</h1>, document.getElementById('example'));
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/scripts/main.js'
+  },
+  output: {
+    filename: './dist/scripts/[name].js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css-loader!sass-loader')
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('dist/styles/main.css', {
+      allChunks: true
+    })
+  ]
+};
